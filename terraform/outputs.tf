2769f3f1503b1b0b6d32387cd91f69fea32628b6
yukpiz <yukpiz@gmail.com>
2026-05-15 16:30:14 +0900

    feat: data_source_id を terraform output に追加

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -8,6 +8,11 @@ output "datasource_bucket_name" {
   value       = aws_s3_bucket.datasource.id
 }
 
+output "data_source_id" {
+  description = "Bedrock Knowledge Base data source ID"
+  value       = aws_bedrockagent_data_source.this.data_source_id
+}
+
 output "vector_bucket_name" {
   description = "S3 vector bucket name"
   value       = aws_s3vectors_vector_bucket.this.vector_bucket_name
